ebc53e733a36ce0b58d8f52d337625feac82478b
Diego Lima de Oliveira <diego.dromer@hotmail.com>
2022-04-23 04:06:24 -0300

    Módulo da biblioteca de animação de imagens adicionado e Tela de Favoritos finalizada

---
+++ autoimageslider/src/main/java/com/smarteist/autoimageslider/Transformations/SimpleTransformation.java
@@ -0,0 +1,11 @@
+package com.smarteist.autoimageslider.Transformations;
+
+import com.smarteist.autoimageslider.SliderPager;
+import android.view.View;
+
+public class SimpleTransformation implements SliderPager.PageTransformer {
+    @Override
+    public void transformPage(View page, float position) {
+
+    }
+}
